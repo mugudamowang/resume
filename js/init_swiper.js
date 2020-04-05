203b0c55a467a6c1a95ba8687e4632980c623ad6
mugu <1175233520@qq.com>
2020-04-05 12:59:11 +0800

    -v

--- js/init_swiper.js
+++ js/init_swiper.js
@@ -1,13 +1,15 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-    },
-})
+!function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+    })
+}.call()
--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -1,50 +1,52 @@
-window.onscroll = function () {
-    if (window.scrollY > 0) {
-        topNavBar.classList.add('sticky')
-        
-        let highLightTargets = document.querySelectorAll("[data-highlight]")
-        let index = 0;
-
-        for (let i = 0; i < highLightTargets.length; i++) {
-            highLightTargets[i].classList.remove('slideinSoft')
-            if (Math.abs(highLightTargets[i].offsetTop - window.scrollY) < Math.abs(highLightTargets[index].offsetTop - window.scrollY)) {
-                index = i
+!function(){
+    window.onscroll = function () {
+        if (window.scrollY > 0) {
+            topNavBar.classList.add('sticky')
+            
+            let highLightTargets = document.querySelectorAll("[data-highlight]")
+            let index = 0;
+    
+            for (let i = 0; i < highLightTargets.length; i++) {
+                highLightTargets[i].classList.remove('slideinSoft')
+                if (Math.abs(highLightTargets[i].offsetTop - window.scrollY) < Math.abs(highLightTargets[index].offsetTop - window.scrollY)) {
+                    index = i
+                }
             }
+            highLightTargets[index].classList.add('slideinSoft')
+            let highLightId = highLightTargets[index].id
+            let highlightNav = document.querySelector('a[href ="#' + highLightId + '"]')
+            let brothersNav = highlightNav.parentNode.parentNode.children
+            for (let i = 0; i < brothersNav.length - 1; i++) {
+                brothersNav[i].classList.remove('highlight')
+            }
+            highlightNav.parentNode.classList.add('highlight')
+        } else {
+            topNavBar.classList.remove('sticky')
         }
-        highLightTargets[index].classList.add('slideinSoft')
-        let highLightId = highLightTargets[index].id
-        let highlightNav = document.querySelector('a[href ="#' + highLightId + '"]')
-        let brothersNav = highlightNav.parentNode.parentNode.children
-        for (let i = 0; i < brothersNav.length - 1; i++) {
-            brothersNav[i].classList.remove('highlight')
-        }
-        highlightNav.parentNode.classList.add('highlight')
-    } else {
-        topNavBar.classList.remove('sticky')
     }
-}
-
-let loadcateTargets = document.querySelectorAll('#topNavBar #navBarul > li > a')
-function animate(time) { requestAnimationFrame(animate); TWEEN.update(time); }
-requestAnimationFrame(animate)
-for (let i = 0; i < loadcateTargets.length; i++) {
-    loadcateTargets[i].onclick = function (e) {
-        e.preventDefault()
-        let href = e.currentTarget.getAttribute('href')
-        let currentTop = window.scrollY
-        let TargetTop = 0
-        if (document.querySelector(href)) {
-            TargetTop = document.querySelector(href).offsetTop - 100
+    
+    let loadcateTargets = document.querySelectorAll('#topNavBar #navBarul > li > a')
+    function animate(time) { requestAnimationFrame(animate); TWEEN.update(time); }
+    requestAnimationFrame(animate)
+    for (let i = 0; i < loadcateTargets.length; i++) {
+        loadcateTargets[i].onclick = function (e) {
+            e.preventDefault()
+            let href = e.currentTarget.getAttribute('href')
+            let currentTop = window.scrollY
+            let TargetTop = 0
+            if (document.querySelector(href)) {
+                TargetTop = document.querySelector(href).offsetTop - 100
+            }
+            let times = Math.abs(TargetTop - currentTop)
+            if (times > 1000) {
+                times = 1000
+            }
+            const coords = { y: currentTop }
+            const tween = new TWEEN.Tween(coords)
+                .to({ y: TargetTop }, times)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate(() => { window.scrollTo(0, coords.y) })
+                .start()
         }
-        let times = Math.abs(TargetTop - currentTop)
-        if (times > 1000) {
-            times = 1000
-        }
-        const coords = { y: currentTop }
-        const tween = new TWEEN.Tween(coords)
-            .to({ y: TargetTop }, times)
-            .easing(TWEEN.Easing.Quadratic.Out)
-            .onUpdate(() => { window.scrollTo(0, coords.y) })
-            .start()
     }
-}
+}.call()
